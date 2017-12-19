f3d9dc6219ebc389343197afba7b622abaa1a642
Kevin <marquezkrodriguez24@gmail.com>
2017-12-19 09:10:43 -0600

    Version Original v3

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
 var express = require('express');
+
 var app = express();
 
-app.set('view engine','pug')
+app.set('view engine', 'pug');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.render('index', { title:'Platzigram'})
+  res.render('index', { title: 'Platzigram' });
 })
 
 app.get('/signup', function (req, res) {
-  res.render('index', { title:'Platzigram - Signup'});
+  res.render('index', { title: 'Platzigram - Signup' });
 })
 
 app.get('/signin', function (req, res) {
-  res.render('index', { title:'Platzigram - Signin'});
+  res.render('index', { title: 'Platzigram - Signin' });
 })
 
-app.listen(3000,function(err){
-	if(err) return console.log('Hay un error..!'), process.exit(1)
+app.listen(3000, function (err) {
+  if (err) return console.log('Hubo un error'), process.exit(1);
 
-	console.log('Platzigram escuchando en el puerto 3000')
-})	
-
-
-
-
+  console.log('Platzigram escuchando en el puerto 3000');
+})
--- src/homepage/index.js
+++ src/homepage/index.js
@@ -1,8 +1,6 @@
 var page = require('page');
-var title = require('title')
 
 page('/', function (ctx, next) {
-	title('Platzigram')
   var main = document.getElementById('main-container');
-  main.innerHTML = '<a href="/signup">Signup</a>'
+  main.innerHTML = '<a href="/signup">Signup</a>';
 })
