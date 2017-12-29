e81efcbb00569c10afd895d5984bdfa71e916d4b
Kmarquez17 <marquezkrodriguez24@gmail.com>
2017-12-28 22:53:59 -0600

    Version Original v6

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -6,5 +6,27 @@ var title = require('title');
 page('/', function (ctx, next) {
   title('Platzigram');
   var main = document.getElementById('main-container');
-  empty(main).appendChild(template);
+
+  var pictures = [
+  	{
+  		user:{
+  			username: 'Kevin',
+  			avatar: 'https://scontent.fmga2-1.fna.fbcdn.net/v/t1.0-9/13178985_986721878072500_655539480996127349_n.jpg?oh=93bc18796f85886646ec6290811737ac&oe=5AB080CB'
+  		},
+  		url: 'https://scontent.fmga2-1.fna.fbcdn.net/v/t1.0-9/13178985_986721878072500_655539480996127349_n.jpg?oh=93bc18796f85886646ec6290811737ac&oe=5AB080CB',
+  		likes: 2,
+  		liked:true
+  	},
+  	{
+  		user:{
+  			username: 'Alberto',
+  			avatar: 'https://scontent.fmga2-1.fna.fbcdn.net/v/t1.0-9/13177645_986721941405827_6565890482379551510_n.jpg?oh=2acd8aa4358072dddfe79b05e8c95c68&oe=5AC35F0C'
+  		},
+  		url: 'https://scontent.fmga2-1.fna.fbcdn.net/v/t1.0-9/13177645_986721941405827_6565890482379551510_n.jpg?oh=2acd8aa4358072dddfe79b05e8c95c68&oe=5AC35F0C',
+  		likes: 10,
+  		liked:true
+  	}
+  ];
+
+  empty(main).appendChild(template(pictures));
 })
--- src/homepage/template.js
+++ src/homepage/template.js
@@ -4,12 +4,18 @@
 //parametro y este sea llamada en otra paginación.
 var yo = require('yo-yo');
 var layout = require('../layout')
-
-var template = yo`<div class="container timeline">
+var picture = require('../picture-card')
+ 
+module.exports = function(pictures){
+	var el = yo`<div class="container timeline">
 	<div class="row">
 		<div class="col s12 m10 offset-m1 l6 offset-l3">
-			 <p>content</p>
+			 ${pictures.map(function(pic){
+			 	return picture(pic);
+			 })}
 		</div>
 	</div>
 </div>`
-module.exports = layout(template);
+
+	return layout(el); 
+}
