f595583a47f3a4c3b7febac611a01a1d02ddaacb
Kmarquez17 <marquezkrodriguez24@gmail.com>
2017-12-29 22:42:23 -0600

    Version Original v7

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -15,7 +15,8 @@ page('/', function (ctx, next) {
   		},
   		url: 'https://scontent.fmga2-1.fna.fbcdn.net/v/t1.0-9/13178985_986721878072500_655539480996127349_n.jpg?oh=93bc18796f85886646ec6290811737ac&oe=5AB080CB',
   		likes: 2,
-  		liked:true
+  		liked:false,
+      createdAt: new Date()
   	},
   	{
   		user:{
@@ -24,7 +25,8 @@ page('/', function (ctx, next) {
   		},
   		url: 'https://scontent.fmga2-1.fna.fbcdn.net/v/t1.0-9/13177645_986721941405827_6565890482379551510_n.jpg?oh=2acd8aa4358072dddfe79b05e8c95c68&oe=5AC35F0C',
   		likes: 10,
-  		liked:true
+  		liked:true,
+      createdAt: new Date().setDate(new Date().getDate() -10)
   	}
   ];
 
